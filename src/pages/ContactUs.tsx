8169a7d093cd22d4ae7964c4b1bae9e3a2a4482e
savez2223 <mosavez0786@gmail.com>
2024-12-27 09:35:27 +0530

    update

--- src/pages/ContactUs.tsx
+++ src/pages/ContactUs.tsx
@@ -153,7 +153,7 @@ const ContactUs: React.FC = () => {
                   rel="noopener noreferrer"
                   className="hover:text-primary transition-colors"
                 >
-                  Sector 86 , Gurugram, Haryana 122052
+                  Sector 86 , Gurugram, H
                 </a>
               </li>
             </ul>
@@ -161,7 +161,7 @@ const ContactUs: React.FC = () => {
             {/* Map */}
             <div className="mt-8 w-full h-64">
               <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2796.7514312422777!2d76.936514!3d28.399672!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d3dedc3cc1b61%3A0x5475aefef924729e!2s83%2C%20Sector%2086%20Rd%2C%20Sector%2086%2C%20Gurugram%2C%20Haryana%20122052!5e1!3m2!1sen!2sin!4v1735270649281!5m2!1sen!2sin"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2796.7514312422777!2d76.936514!3d28.399672!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d3ded  c3cc1b61%3A0x5475aefef924729e!2s83%2C%20Sector%2086%20Rd%2C%20Sector%2086%2C%20Gurugram%2C%20Haryana%20122052!5e1!3m2!1sen!2sin!4v1735270649281!5m2!1sen!2sin"
                 className="w-full h-full"
                 style={{ border: 0 }}
                 allowFullScreen={true}
